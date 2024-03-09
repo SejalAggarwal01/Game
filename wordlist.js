527023f74cca66a4c38487aad7903c79f60ce049
Sejal <sejalaggarwal@101>
2024-03-09 20:46:55 +0530

    Initial Commit

---
+++ wordlist.js
@@ -0,0 +1,13 @@
+// Check the source code for the questions
+const wordList = [
+    {
+      word: "savings",
+      hint: "This is money you set aside for future use. ",
+    },
+    {
+      word: "portfolio",
+      hint: "This shows the overall value of all your investments.",
+    },
+    
+  ];
+  
